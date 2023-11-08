ffd8a6e67d0255818f5c38e4e87e8cd0710b8ea1
Matt Pryor <matt@stackhpc.com>
2023-11-08 12:07:57 +0000

    Changes to registrar for Pydantic v2

--- registrar/zenith/registrar/config.py
+++ registrar/zenith/registrar/config.py
@@ -19,15 +19,15 @@ class SSHPublicKeyType(str, enum.Enum):
     ED25519  = "ssh-ed25519"
 
 
-class RegistrarConfig(Configuration):
+class RegistrarConfig(
+    Configuration,
+    default_path = "/etc/zenith/registrar.yaml",
+    path_env_var = "ZENITH_REGISTRAR_CONFIG",
+    env_prefix = "ZENITH_REGISTRAR"
+):
     """
     Configuration model for the zenith-registrar package.
     """
-    class Config:
-        default_path = "/etc/zenith/registrar.yaml"
-        path_env_var = "ZENITH_REGISTRAR_CONFIG"
-        env_prefix = "ZENITH_REGISTRAR"
-
     #: The key that is used to sign the subdomain tokens
     subdomain_token_signing_key: conbytes(strip_whitespace = True, min_length = 32)
 
@@ -51,7 +51,7 @@ class RegistrarConfig(Configuration):
     generate_domain_max_attempts: conint(gt = 0) = 3
 
     #: The set of allowed SSH key types
-    ssh_allowed_key_types: conset(SSHPublicKeyType, min_items = 1) = Field(
+    ssh_allowed_key_types: conset(SSHPublicKeyType, min_length = 1) = Field(
         default_factory = lambda: {
             # By default, DSA keys are not permitted
             # SSHPublicKeyType.DSA,
